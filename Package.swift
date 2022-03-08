9045a2ee617a70edd6d866d374bc39fb0fddb8bc
Sergey Romanenko <70233726+purpln@users.noreply.github.com>
2022-03-08 03:27:39 +0300

    no message

--- Package.swift
+++ Package.swift
@@ -6,5 +6,5 @@ let package = Package(
     name: "app",
     products: [.executable(name: "app", targets: ["app"])],
     dependencies: [.package(url: "https://github.com/purpln/libusb.git", .branch("main"))],
-    targets: [.target(name: "app", dependencies: [.product(name: "clibusb", package: "libusb")])]
+    targets: [.target(name: "app", dependencies: [.product(name: "clibusb", package: "libusb")], linkerSettings: [.linkedLibrary("usb-1.0", .when(platforms: [.linux]))])]
 )
