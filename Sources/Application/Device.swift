a4a393dbf247f8a64e4e90477322dc4565f935ff
Sergey Romanenko <70233726+purpln@users.noreply.github.com>
2023-01-25 16:20:51 +0300

    no message

--- Sources/Application/Device.swift
+++ Sources/Application/Device.swift
@@ -16,8 +16,8 @@ struct Device {
         self.pointer = pointer
         self.descriptor = descriptor
         self.config = config[0]
-        print("config:", config[0].description)
-        print("descriptor:", descriptor, terminator: "\n\n")
+        print("{\"config\": \(config[0].description), \"descriptor\": \(descriptor)}")
+        print("", terminator: "\n")
     }
 }
 
@@ -108,39 +108,39 @@ extension Device {//size - buffer size
 extension libusb_device_descriptor: CustomStringConvertible {
     public var description: String {
         var values: [String] = []
-        values.append(["bLength", String(bLength, radix: 16)].joined(separator: ": "))
-        values.append(["bDescriptorType", String(bDescriptorType, radix: 16)].joined(separator: ": "))
-        values.append(["bcdUSB", String(bcdUSB, radix: 16)].joined(separator: ": "))
-        values.append(["bDeviceClass", String(bDeviceClass, radix: 16)].joined(separator: ": "))
-        values.append(["bDeviceSubClass", String(bDeviceSubClass, radix: 16)].joined(separator: ": "))
-        values.append(["bDeviceProtocol", String(bDeviceProtocol, radix: 16)].joined(separator: ": "))
-        values.append(["bMaxPacketSize0", String(bMaxPacketSize0, radix: 16)].joined(separator: ": "))
-        values.append(["idVendor", String(idVendor, radix: 16)].joined(separator: ": "))
-        values.append(["idProduct", String(idProduct, radix: 16)].joined(separator: ": "))
-        values.append(["bcdDevice", String(bcdDevice, radix: 16)].joined(separator: ": "))
-        values.append(["iManufacturer", String(iManufacturer, radix: 16)].joined(separator: ": "))
-        values.append(["iProduct", String(iProduct, radix: 16)].joined(separator: ": "))
-        values.append(["iSerialNumber", String(iSerialNumber, radix: 16)].joined(separator: ": "))
-        values.append(["bNumConfigurations", String(bNumConfigurations, radix: 16)].joined(separator: ": "))
-        return "(" + values.joined(separator: ", ") + ")"
+        values.append("\"bLength\": \"\(String(bLength, radix: 16))\"")
+        values.append("\"bDescriptorType\": \"\(String(bDescriptorType, radix: 16))\"")
+        values.append("\"bcdUSB\": \"\(String(bcdUSB, radix: 16))\"")
+        values.append("\"bDeviceClass\": \"\(String(bDeviceClass, radix: 16))\"")
+        values.append("\"bDeviceSubClass\": \"\(String(bDeviceSubClass, radix: 16))\"")
+        values.append("\"bDeviceProtocol\": \"\(String(bDeviceProtocol, radix: 16))\"")
+        values.append("\"bMaxPacketSize0\": \"\(String(bMaxPacketSize0, radix: 16))\"")
+        values.append("\"idVendor\": \"\(String(idVendor, radix: 16))\"")
+        values.append("\"idProduct\": \"\(String(idProduct, radix: 16))\"")
+        values.append("\"bcdDevice\": \"\(String(bcdDevice, radix: 16))\"")
+        values.append("\"iManufacturer\": \"\(String(iManufacturer, radix: 16))\"")
+        values.append("\"iProduct\": \"\(String(iProduct, radix: 16))\"")
+        values.append("\"iSerialNumber\": \"\(String(iSerialNumber, radix: 16))\"")
+        values.append("\"bNumConfigurations\": \"\(String(bNumConfigurations, radix: 16))\"")
+        return "{" + values.joined(separator: ", ") + "}"
     }
 }
 
 extension libusb_config_descriptor: CustomStringConvertible {
     public var description: String {
         var values: [String] = []
-        values.append("bLength: " + String(bLength, radix: 16))
-        values.append("bDescriptorType: " + String(bDescriptorType, radix: 16))
-        values.append("wTotalLength: " + String(wTotalLength, radix: 16))
-        values.append("bConfigurationValue: " + String(bConfigurationValue, radix: 16))
-        values.append("iConfiguration: " + String(iConfiguration, radix: 16))
-        values.append("bmAttributes: " + String(bmAttributes, radix: 16))
-        values.append("MaxPower: " + String(MaxPower, radix: 16))
-        values.append("interfaces: " + "[" + interfaces.map(\.description).joined(separator: ", ") + "]")
+        values.append("\"bLength\": \"\(String(bLength, radix: 16))\"")
+        values.append("\"bDescriptorType\": \"\(String(bDescriptorType, radix: 16))\"")
+        values.append("\"wTotalLength\": \"\(String(wTotalLength, radix: 16))\"")
+        values.append("\"bConfigurationValue\": \"\(String(bConfigurationValue, radix: 16))\"")
+        values.append("\"iConfiguration\": \"\(String(iConfiguration, radix: 16))\"")
+        values.append("\"bmAttributes\": \"\(String(bmAttributes, radix: 16))\"")
+        values.append("\"MaxPower\": \"\(String(MaxPower, radix: 16))\"")
+        values.append("\"interfaces\": [\(interfaces.map(\.description).joined(separator: ", "))]")
         if let bytes = bytes {
-            values.append("bytes: " + "[" + bytes.map(\.description).joined(separator: ", ") + "]")
+            values.append("bytes: [\(bytes.map(\.description).joined(separator: ", "))]")
         }
-        return "(" + values.joined(separator: ", ") + ")"
+        return "{" + values.joined(separator: ", ") + "}"
     }
 }
 
@@ -153,19 +153,19 @@ extension libusb_interface: CustomStringConvertible {
 extension libusb_interface_descriptor: CustomStringConvertible {
     public var description: String {
         var values: [String] = []
-        values.append(["bLength", String(bLength, radix: 16)].joined(separator: ": "))
-        values.append(["bDescriptorType", String(bDescriptorType, radix: 16)].joined(separator: ": "))
-        values.append(["bInterfaceNumber", String(bInterfaceNumber, radix: 16)].joined(separator: ": "))
-        values.append(["bAlternateSetting", String(bAlternateSetting, radix: 16)].joined(separator: ": "))
-        values.append(["bNumEndpoints", String(bNumEndpoints, radix: 16)].joined(separator: ": "))
-        values.append(["bInterfaceClass", String(bInterfaceClass, radix: 16)].joined(separator: ": "))
-        values.append(["bInterfaceSubClass", String(bInterfaceSubClass, radix: 16)].joined(separator: ": "))
-        values.append(["bInterfaceProtocol", String(bInterfaceProtocol, radix: 16)].joined(separator: ": "))
-        values.append(["iInterface", String(iInterface, radix: 16)].joined(separator: ": "))
+        values.append("\"bLength\": \"\(String(bLength, radix: 16))\"")
+        values.append("\"bDescriptorType\": \"\(String(bDescriptorType, radix: 16))\"")
+        values.append("\"bInterfaceNumber\": \"\(String(bInterfaceNumber, radix: 16))\"")
+        values.append("\"bAlternateSetting\": \"\(String(bAlternateSetting, radix: 16))\"")
+        values.append("\"bNumEndpoints\": \"\(String(bNumEndpoints, radix: 16))\"")
+        values.append("\"bInterfaceClass\": \"\(String(bInterfaceClass, radix: 16))\"")
+        values.append("\"bInterfaceSubClass\": \"\(String(bInterfaceSubClass, radix: 16))\"")
+        values.append("\"bInterfaceProtocol\": \"\(String(bInterfaceProtocol, radix: 16))\"")
+        values.append("\"iInterface\": \"\(String(iInterface, radix: 16))\"")
         if let bytes = bytes {
-            values.append("bytes: " + "[" + bytes.map(\.description).joined(separator: ", ") + "]")
+            values.append("\"bytes\": [\(bytes.map(\.description).joined(separator: ", "))]")
         }
-        return "(" + values.joined(separator: ", ") + ")"
+        return "{" + values.joined(separator: ", ") + "}"
     }
 }
 
